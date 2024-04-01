e336f44de35ef48acb8253a606c7688bb26a9059
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-04-02 05:26:28 +0800

    boilerplate for taking in the character sprite sheets

--- main.lua
+++ main.lua
@@ -1,21 +1,42 @@
 function love.load()
+	-- libraries
 	Object = require "lib.classic"
 
+	-- local imports
+
+	-- screen & bg work
 	screen_width = love.graphics.getWidth()
 	screen_height = love.graphics.getHeight()
 
-	bg = love.graphics.newImage('visuals/background.png')
+	bg = love.graphics.newImage("visuals/background.png")
 	bg_width = bg:getWidth()
 	bg_height = bg:getHeight()
 	bg_scaleX = screen_width / bg_width
 	bg_scaleY = screen_height / bg_height
 
-	print(bg_scaleX)
-	print(bg_scaleY)
-
 	bg_music = love.audio.newSource("audio/bgm.mp3", "stream")
 	bg_music:setLooping(true)
 	love.audio.play(bg_music)
+
+	-- sprite work
+	-- both sheets have the same amt of frames and arrangement (missing 5th frame at 2nd and 4th row)
+	-- (speaking of which) both sheets have 5 columns and 4 rows, each row depicting the same action
+	-- both sheets also have the same width and so also same frame width and frame height
+	maxFrames = 18
+
+	charRawSheet = love.graphics.newImage("visuals/char1sheet.png")
+	charRawSheetWidth = charRawSheet:getWidth()
+	charRawSheetHeight = charRawSheet:getHeight()
+	rawFrameWidth = charRawSheetWidth/5
+	rawFrameHeight = charRawSheetHeight/4
+	charRawFrames = {}
+
+	charCostumeSheet = love.graphics.newImage("visuals/char2sheet.png")
+	charCostumeSheetWidth = charCostumeSheet:getWidth()
+	charCostumeSheetHeight = charCostumeSheet:getHeight()
+	costumeFrameWidth = charCostumeSheetWidth/5
+	costumeFrameHeight = charCostumeSheetHeight/4
+	charCostumeFrames = {}
 end
 
 function love.update(dt)
