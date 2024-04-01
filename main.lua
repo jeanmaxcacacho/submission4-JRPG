f70fd43c47e26716557eea6db736e9fbfd04617b
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-04-01 20:40:41 +0800

    drew background image

--- main.lua
+++ main.lua
@@ -1,5 +1,15 @@
 function love.load()
+	screen_width = love.graphics.getWidth()
+	screen_height = love.graphics.getHeight()
 
+	bg = love.graphics.newImage('visuals/background.png')
+	bg_width = bg:getWidth()
+	bg_height = bg:getHeight()
+	bg_scaleX = screen_width / bg_width
+	bg_scaleY = screen_height / bg_height
+
+	print(bg_scaleX)
+	print(bg_scaleY)
 end
 
 function love.update(dt)
@@ -7,5 +17,5 @@ function love.update(dt)
 end
 
 function love.draw()
-
+	love.graphics.draw(bg, 0, 0, 0, bg_scaleX, bg_scaleY)
 end
