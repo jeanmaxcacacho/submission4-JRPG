76e81fe58d9a6fca5614b1eb1e5c2f975c0d9d6b
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-04-02 20:57:11 +0800

    nvm let's work on turn queue first

--- src/menu.lua
+++ src/menu.lua
@@ -1,7 +1,10 @@
 Menu = Object:extend()
 
 function Menu:new()
-
+	self.width = 800
+	self.height = 250
+	self.x = love.graphics.getWidth()/2 - self.width/2
+	self.y = 50
 end
 
 -- this is just going to keep calling the "event listeners" bullshit
@@ -10,5 +13,7 @@ function Menu:update(dt)
 end
 
 function Menu:draw()
-
+	love.graphics.setColor(0.6, 0.6, 0, 0.5)
+	love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+	love.graphics.setColor(1, 1, 1, 1)
 end
