6e94ca26b0754d6e760994faeea2e45533d33033
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-04-02 20:33:38 +0800

    I made the file but like idk what to do

---
+++ src/menu.lua
@@ -0,0 +1,14 @@
+Menu = Object:extend()
+
+function Menu:new()
+
+end
+
+-- this is just going to keep calling the "event listeners" bullshit
+function Menu:update(dt)
+
+end
+
+function Menu:draw()
+
+end
